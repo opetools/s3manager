da024ea012e9d51e533f7f90daea7faca91be548
taku.ohkanda <bigkander@gmail.com>
2020-02-05 22:11:43 +0900

    init commit

--- src/app/constant.ts
+++ src/app/constant.ts
@@ -1,8 +1,10 @@
 export const SESSION_KEY_ACCESS_KEY = 's3manager.accessKey';
+
 export const LOCAL_KEY_REGION = 's3manager.region';
 export const LOCAL_KEY_LOGIN_TYPE = 's3manager.loginType';
 export const LOCAL_KEY_LOGIN_INDEX = 's3manager.loginIndex';
 export const LOCAL_KEY_BUCKETS = 's3manager.buckets';
+
 export const enum LOGIN_TYPE {
   cognito = '0',
   accessKey = '1',
